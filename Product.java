173bf49ab4cf872b584055e73ef0b7873445177b
tergelldd <82456469+tergelldd@users.noreply.github.com>
2025-06-04 11:34:36 +0800

    Product.java
    zasav

--- Product.java
+++ Product.java
@@ -1,19 +1,13 @@
 public class Product {
     private int id;
     private String name;
-    private String category;
     private int quantity;
     private double price;
 
-    // Constructor
-    public Product(int id, String name, String category, int quantity, double price) {
+    public Product(int id, String name, int quantity, double price) {
         this.id = id;
         this.name = name;
-        this.category = category;
         this.quantity = quantity;
         this.price = price;
     }
-
-    // Getters and Setters
-    // toString() method
 }
